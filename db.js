02c83d88a2a3e9b0e75787643520e42026548d41
marbleville <allen.ehrhardt@gmail.com>
2021-10-16 17:30:58 -0400

    updated db

--- db.js
+++ db.js
@@ -1,8 +1,13 @@
+const { CommandInteractionOptionResolver } = require('discord.js');
 const fs = require('fs');
 
 writeClass();
 
 function writeClass(name, roster) {
     //might have to refractor to add karma systems but fo no just sotring names
-    fs.writeFileSync(`./classes/${name}.json`, JSON.stringify(roster));
+    try {
+        fs.writeFileSync(`./classes/${name}.json`, JSON.stringify(roster));
+    } catch(error) {
+        console.log(error);
+    }
 }
