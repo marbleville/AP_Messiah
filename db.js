0d48e1c2c83df310676ae012c673fb7b08657c8a
marbleville <allen.ehrhardt@gmail.com>
2021-10-16 23:35:38 -0400

    added new db functions

--- db.js
+++ db.js
@@ -1,13 +1,21 @@
 const { CommandInteractionOptionResolver } = require('discord.js');
 const fs = require('fs');
 
-writeClass();
-
 function writeClass(name, roster) {
-    //might have to refractor to add karma systems but fo no just sotring names
+    //might have to refractor to add karma systems but for now just sotring names
     try {
         fs.writeFileSync(`./classes/${name}.json`, JSON.stringify(roster));
     } catch(error) {
         console.log(error);
     }
+}
+
+function getClass(name) {
+    let c = undefined;
+    fs.readdirSync().forEach(file => {
+        if (file === name) {
+            c = JSON.parse(fs.readFileSync(`./classes/${file}`, 'utf8'));
+        }
+    })
+    return c;
 }
