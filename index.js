3c2c40e07e8226399dae22205472d939135e226c
marbleville <allen.ehrhardt@gmail.com>
2021-10-17 16:49:23 -0400

    Info update

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ bot.on('interactionCreate', interaction => {
                     .setTitle('**Info about AP Discord Server**')
                     .addField('Get Help:', 'This server serves as a hub for study materials and help from fellow AP students.')
                     .addField('Features:', 'This bot tracks students in this server of all AP classes offered by MHS with `/classes`, `/enroll`, and `/unenroll`. It also keeps track of different study materials that members have posted with `/study`')
-                    .addField('\u200B', 'Bot written by <@464156671024037918>. AP Discord Server managed by <@703028154431832094>. Credit for original idea goes to <@371318217454387211>. ')
+                    .addField('\u200B', 'AP Discord Server managed by <@703028154431832094>. Bot written by <@464156671024037918>. Credit for original idea goes to <@371318217454387211>. ')
                 interaction.reply({ embeds: [Embed3] });
             break;
         }
